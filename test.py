c63d3e0e94a27f4eedbc492772fafc018553de40
lv101 <66996203+lv101@users.noreply.github.com>
2020-06-18 18:55:50 +0800

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import re
-text = '"title":"请问：平光是谁？","content":"娘搞后来，就被笑话到了现在……"'
-title = re.findall(r'"title":"(.*?)"',text)[0]
-content = re.findall(r'"content":"(.*?)"', text)[0]
-print(f"{title}\n{content}")
