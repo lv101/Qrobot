ead3489825d18d62c00f622371e6e31b2903ecc1
lv101 <lv101@users.noreply.github.com>
2020-06-18 19:46:26 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,40 @@
 import moli
+# from clock import judge_0
 import tuling
 import plugins
+import threading
 from commands import cmds
-
-
 from cqhttp import CQHttp
 
+# config中 "post_url": "http://127.0.0.1:5701"
 
 bot = CQHttp(api_root='http://127.0.0.1:5700',
              access_token="L",
              secret="2333"
              )
 
-@bot.on_message('private')
-def handle_message(context):
+def handle_all_message(context):
     message: str = context['message']
     msg = message.split(maxsplit=1)
     if not msg:
         return ''
     cmd, *args = msg
     arg = ''.join(args)
-    handler = cmds.get(cmd)
+
+    if cmd[0] in ['\\', '/', '／']:
+        handler = cmds.get(cmd[1:])
+    else:
+        handler = None
+
     print('cmd:', cmd)
     print('arg:', arg)
     print('handler:', handler)
 
-    if cmd[0] == '\\':
-        handler = cmds.get(cmd[1:])
-    else:
-        handler = None
+    return arg, message, handler
+
+@bot.on_message('private')
+def handle_message(context):
+    arg, message, handler = handle_all_message(context)
 
     if handler:
         return handler(context, arg)
@@ -44,26 +50,11 @@ def handle_message(context):
 
 @bot.on_message('group')
 def handle_message(context):
-    message: str = context['message']
-    msg = message.split(maxsplit=1)
-    if not msg:
-        return ''
-    cmd, *args = msg
-    arg = ''.join(args)
-    if cmd[0] == '\\':
-        handler = cmds.get(cmd[1:])
-    else:
-        handler = None
-    print('cmd:', cmd)
-    print('arg:', arg)
-    print('handler:', handler)
-    print(context)
+    arg, message, handler = handle_all_message(context)
 
     if handler:
         return handler(context, arg)
     elif '[CQ:at,qq=3457292188]' in message:
-        if context['user_id'] == 296491216:
-            return {'reply': "您说的对！"}
         replies = tuling.get_reply(message)
         if replies[0] == '请求次数超限制!':
             reply = moli.get_reply(message)
@@ -71,5 +62,8 @@ def handle_message(context):
             return {'reply': reply}
         else:
             return {'reply': replies[0]}
+
+# t1 = threading.Thread(target=judge_0)
+# t1.start()
 
 bot.run(host='127.0.0.1', port=5701, debug=True)
