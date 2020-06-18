0a2ff20f9d3f78d59a0bddc57115b6702c069725
lv101 <lv101@users.noreply.github.com>
2020-06-18 19:45:58 +0800

    Update moli.py

--- moli.py
+++ moli.py
@@ -1,28 +1,26 @@
+'''
+茉莉机器人模块,解决图灵机器人对话次数上限问题
+'''
 import re
-import time
 import requests
 
 def get_reply(message):
     payload = {'question': message,
                'limit': 6,
-               'Api_Key': '',
-               'Api_Secret': ''
+               'Api_Key': '40103ccd595f5151a664c8db2fc54aa2',
+               'Api_Secret': 'zg9tykwdaj5y'
                }
 
     url = 'http://i.itpk.cn/api.php'
     r = requests.get(url, params=payload)
 
-    if re.findall(r".?笑话", message):
-        text = r.content.decode('unicode-escape').split('"')
-        title = text[3]
-        content = text[-2]
-
-        # title = re.findall(r'"title":"(.*?)"', text)
-        # content = re.findall(r'"content":"(.*?)"', text)
-        print(text)
+    if re.findall(r"笑话", message):
+        text = r.content.decode('unicode-escape')
+        title = re.findall(r'"title":"(.*?)"', text, re.S)[0]
+        content = re.findall(r'"content":"(.*?)"', text, re.S)[0]
         if title and content:
             return f"《{title}》\n{content}"
         else:
-            return "人家刚才打盹了呢,请再说一遍吧 >_<"
+            return "人家刚才打盹了呢,再说一次吧 >_<"
     else:
         return r.text
