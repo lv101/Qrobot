02518dcb352f2b42fa942fe0837609e7e61f6d75
lv101 <lv101@users.noreply.github.com>
2020-06-18 19:44:13 +0800

    Update local_flask.py

--- local_flask.py
+++ local_flask.py
@@ -1,23 +1,19 @@
-from flask import Flask,request
 from json import loads
-import json
+
+from flask import Flask, request
 
 bot_server = Flask(__name__)
 
 @bot_server.route('/', methods=['POST'])
 def server():
-    # data = request.get_data().decode('utf-8')
-    # data = loads(data)
-    # QQ = data.get('user_id')
-    # nickname = data['sender'].get('nickname')
-    # message = data['message'][0]['data'].get('text')
-    # print(f"{QQ}:{nickname}:{message}")
-    user = {
-        "username": "L",
-        "nickname": "张三"
-    }
-    return json.dumps(user)
-
+    # data = {}
+    data = request.get_data().decode('utf-8')
+    data = loads(data)
+    QQ = data.get('user_id')
+    nickname = data['sender'].get('nickname')
+    message = data['message']
+    print(f"{QQ}:{nickname}:{message}")
+    return ''
 
 if __name__ == '__main__':
-    bot_server.run(host='127.0.0.1', port=9090, debug=True)
+    bot_server.run(host='127.0.0.1', port=5701, debug=True)
